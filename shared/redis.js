ce531a860227811193357620ff97d27c069af2a2
SHREYAS V CHAVAN <152523720+ShreyasChavan1@users.noreply.github.com>
2025-10-29 19:54:34 +0530

    Refactor Redis connection setup
    Refactor Redis connection initialization to use host, port, and password from environment variables.

--- shared/redis.js
+++ shared/redis.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const Ioredis = require("ioredis");
 
-
-const redisConnection = new Ioredis(process.env.REDIS_HOST /* or REDIS_URL */, {
+const redisConnection = new Ioredis({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD,
   maxRetriesPerRequest: null,
-  // only include TLS if your URL is rediss://
-  // tls: { rejectUnauthorized: false }
+  // tls: { rejectUnauthorized: false } // only if using rediss://
 });
 
 module.exports = redisConnection;
