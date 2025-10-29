4c0030b99e33aea9670341a74976454863c8a032
SHREYAS V CHAVAN <152523720+ShreyasChavan1@users.noreply.github.com>
2025-10-29 19:47:58 +0530

    Refactor redisConnection setup for flexibility
    Updated redisConnection initialization to support REDIS_URL.

--- shared/redis.js
+++ shared/redis.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const Ioredis = require("ioredis");
 
-const redisConnection = new Ioredis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    maxRetriesPerRequest: null
+
+const redisConnection = new Ioredis(process.env.REDIS_HOST /* or REDIS_URL */, {
+  maxRetriesPerRequest: null,
+  // only include TLS if your URL is rediss://
+  // tls: { rejectUnauthorized: false }
 });
 
 module.exports = redisConnection;
