6ef7677928b5243cc99565586131d16d1ecc9d26
SHREYAS V CHAVAN <152523720+ShreyasChavan1@users.noreply.github.com>
2025-10-29 21:08:59 +0530

    Fix typo in redis connection password property

--- shared/redis.js
+++ shared/redis.js
@@ -3,6 +3,7 @@ const Ioredis = require("ioredis");
 
 console.log("REDIS_HOST =", process.env.REDIS_HOST); 
 const redisConnection = new Ioredis(process.env.REDIS_HOST, {
+  passwoard:process.env.REDIS_PASSWORD,
   maxRetriesPerRequest: null,
   tls: { rejectUnauthorized: false } // keep this if Railway uses rediss://
 });
